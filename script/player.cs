c47662814db522ce175eadbc9d634d4ba8ef17de
LPotasiewicz <lpotasiewicz@ccsuvt.org>
2017-05-31 22:25:19 -0400

    initital commit

---
+++ script/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 movement = new Vector2(rb2d.velocity.x, rb2d.velocity.y);
+		if (Input.GetKey (KeyCode.A)) {
+			movement.x = -speed;
+		} else if (Input.GetKey (KeyCode.D)) {
+			movement.x = speed;
+		}
+		rb2d.velocity = new Vector2(movement.x, movement.y);
+	}
+}
