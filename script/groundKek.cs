ab7a7c3ea8a898847d6dcbb123800788554aa185
LPotasiewicz <lpotasiewicz@ccsuvt.org>
2017-06-01 19:26:45 -0400

    movement and animation - basic

---
+++ script/groundKek.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundKek : MonoBehaviour {
+
+	void OnTriggerStay2D (Collider2D other){
+		if (other.gameObject.tag == "ground") {
+			gameObject.GetComponentInParent<player> ().grounded = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag == "ground") {
+			gameObject.GetComponentInParent<player> ().grounded = false;
+		} 
+	}
+}
--- script/player.cs
+++ script/player.cs
@@ -5,21 +5,47 @@ using UnityEngine;
 public class player : MonoBehaviour {
 
 	public Rigidbody2D rb2d;
+	public Animator anim;
 	public float speed;
+	public float jumpHeight;
+	public bool grounded;
+
+	private Vector2 movement;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
 	}
+
 	
 	// Update is called once per frame
-	void Update () {
-		Vector2 movement = new Vector2(rb2d.velocity.x, rb2d.velocity.y);
+	void FixedUpdate () {
+		movement = new Vector2 (rb2d.velocity.x, rb2d.velocity.y);
+		Vector2 direction = new Vector2 (gameObject.transform.localScale.x, 1);
+
+		// WALKING
 		if (Input.GetKey (KeyCode.A)) {
+			direction.x = -1;
 			movement.x = -speed;
 		} else if (Input.GetKey (KeyCode.D)) {
+			direction.x = 1;
 			movement.x = speed;
+		} else {
+			movement.x = rb2d.velocity.x * 0.5f;
 		}
-		rb2d.velocity = new Vector2(movement.x, movement.y);
+
+		// JUMPING
+		if (Input.GetKeyDown (KeyCode.W) && grounded) {
+			movement.y = jumpHeight;
+		}
+
+		// ASSIGN INGAME VARIABLES
+		rb2d.velocity = movement;
+		gameObject.transform.localScale = direction;
+	}
+
+	void Update () {
+		anim.SetBool ("isRunning", (movement.x != 0));
 	}
 }
