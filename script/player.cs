21132ee94315b088536094aa22edc682f360ec9b
LPotasiewicz <lpotasiewicz@ccsuvt.org>
2017-06-01 20:34:40 -0400

    movement tweaks

--- script/player.cs
+++ script/player.cs
@@ -11,6 +11,7 @@ public class player : MonoBehaviour {
 	public bool grounded;
 
 	private Vector2 movement;
+	private bool isRunning;
 
 	// Use this for initialization
 	void Start () {
@@ -24,14 +25,19 @@ public class player : MonoBehaviour {
 		movement = new Vector2 (rb2d.velocity.x, rb2d.velocity.y);
 		Vector2 direction = new Vector2 (gameObject.transform.localScale.x, 1);
 
+		isRunning = false;
 		// WALKING
 		if (Input.GetKey (KeyCode.A)) {
 			direction.x = -1;
 			movement.x = -speed;
-		} else if (Input.GetKey (KeyCode.D)) {
+			isRunning = true;
+		}
+		if (Input.GetKey (KeyCode.D)) {
 			direction.x = 1;
 			movement.x = speed;
-		} else {
+			isRunning = true;
+		} 
+		if (grounded && !isRunning) {
 			movement.x = rb2d.velocity.x * 0.5f;
 		}
 
@@ -46,6 +52,9 @@ public class player : MonoBehaviour {
 	}
 
 	void Update () {
-		anim.SetBool ("isRunning", (movement.x != 0));
+		// ANIMATION VARABLES
+		anim.SetBool ("isRunning", isRunning);
+		anim.SetBool ("grounded", grounded);
+		anim.SetFloat ("yVelocity", movement.y);
 	}
 }
