6adfab91cad6ba01ff6260719e38906b3cc49642
AMountainGoat <timmydaturtle18@gmail.com>
2017-06-05 21:07:47 -0400

    Layers, death animation, health on player

--- script/player.cs
+++ script/player.cs
@@ -10,10 +10,14 @@ public class player : MonoBehaviour {
 	public float jumpHeight;
 	public bool grounded;
 	public GameObject attackObject;
+	public GameObject deathObject;
 	public GameObject attackSpawn;
+	public int health;
 
 	private Vector2 movement;
+	private int attackInstanceID = -1;
 	private bool isRunning;
+	public bool isHit;
 	private bool canAttack = true;
 
 	// Use this for initialization
@@ -22,39 +26,60 @@ public class player : MonoBehaviour {
 		anim = gameObject.GetComponent<Animator> ();
 	}
 
+	// Take Damage
+	void OnTriggerStay2D (Collider2D other){
+		if (other.gameObject.tag == "enemy") {
+			if (other.gameObject.GetComponent<attack> ().GetInstanceID () != attackInstanceID) {
+				attackInstanceID = other.gameObject.GetComponent<attack> ().GetInstanceID ();
+				health -= other.gameObject.GetComponent<attack> ().value;
+				Debug.Log ("ow " + health);
+				StartCoroutine (hitDelay (other.gameObject));
+			}
+		}
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Trigger on Death
+		if (health <= 0) {
+			GameObject deathAnim = Instantiate (deathObject, gameObject.transform.position, Quaternion.identity) as GameObject;
+			Destroy (deathAnim, 1f);
+			Destroy (gameObject);
+		}
 		movement = new Vector2 (rb2d.velocity.x, rb2d.velocity.y);
 		Vector2 direction = new Vector2 (gameObject.transform.localScale.x, 1);
 
 
 		isRunning = false;
 		// WALKING
-		if (Input.GetKey (KeyCode.A)) {
-			direction.x = -1;
-			movement.x = -speed;
-			isRunning = true;
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			direction.x = 1;
-			movement.x = speed;
-			isRunning = true;
-		} 
-		if (grounded && !isRunning) {
-			movement.x = rb2d.velocity.x * 0.5f;
-		}
+		if (!isHit) {
+			if (Input.GetKey (KeyCode.A)) {
+				direction.x = -1;
+				movement.x = -speed;
+				isRunning = true;
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				direction.x = 1;
+				movement.x = speed;
+				isRunning = true;
+			} 
+			if (grounded && !isRunning) {
+				movement.x = rb2d.velocity.x * 0.5f;
+			}
 
-		// JUMPING
-		if (Input.GetKeyDown (KeyCode.W) && grounded) {
-			movement.y = jumpHeight;
-		}
+			// JUMPING
+			if (Input.GetKeyDown (KeyCode.W) && grounded) {
+				movement.y = jumpHeight;
+			}
 
-		// ATTACKING
-		if (Input.GetKeyDown (KeyCode.Space) && canAttack) {
-			GameObject attack = Instantiate (attackObject, attackSpawn.transform.position, Quaternion.identity) as GameObject;
-			Destroy (attack, 0.1f);
-			StartCoroutine (attackDelay ());
+			// ATTACKING
+			if (Input.GetKeyDown (KeyCode.Space) && canAttack) {
+				GameObject attack = Instantiate (attackObject, attackSpawn.transform.position, Quaternion.identity) as GameObject;
+				Destroy (attack, 0.1f);
+				StartCoroutine (attackDelay ());
+			}
 		}
 
 		// ASSIGN INGAME VARIABLES
@@ -73,5 +98,15 @@ public class player : MonoBehaviour {
 		canAttack = false;
 		yield return new WaitForSeconds(0.5f);
 		canAttack = true;
+	}
+
+	IEnumerator hitDelay(GameObject enemy){
+		int direction = (enemy.transform.position.x > gameObject.transform.position.x) ? -1 : 1;
+		rb2d.velocity = new Vector2(direction * 3, 3);
+		isHit = true;
+		yield return new WaitForSeconds(0.3f);
+		isHit = false;
+		yield return new WaitForSeconds(0.3f);
+		attackInstanceID = -1;
 	}
 }
